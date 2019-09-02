23086c9b9325d8b554e386b230409c6991507f31
Marcelo Tocchetto <marcelo.tocchetto@neogrid.com>
2019-09-02 11:01:41 -0300

    Spring sample

---
+++ gateway/src/main/java/com/tocchetto/cloud/gateway/GatewayApplication.java
@@ -0,0 +1,23 @@
+package com.tocchetto.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.tocchetto.cloud.gateway.filters.pre.SimpleFilter;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public SimpleFilter simpleFilter() {
+        return new SimpleFilter();
+    }
+
+}
